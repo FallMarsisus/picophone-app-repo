38e0033e4fccca7ecf860dea77ab43cdc59a1218
Marsisus <simonlncln@gmail.com>
2026-03-07 23:44:34 +0100

    fix: mettre à jour la version à 1.0.13 dans manifest.json

--- apps/trivia/main.py
+++ apps/trivia/main.py
@@ -211,6 +211,299 @@ def shuf(arr):
         arr[j] = tmp
         i = i - 1
 
+def action_sel_cat(idx):
+    global cid
+    cid = cids[idx]
+    mcbs[0].set_style_bg_color(COL_AC, 0)
+    mcbs[0].set_style_border_color(COL_HL, 0)
+    mcbs[1].set_style_bg_color(COL_AC, 0)
+    mcbs[1].set_style_border_color(COL_HL, 0)
+    mcbs[2].set_style_bg_color(COL_AC, 0)
+    mcbs[2].set_style_border_color(COL_HL, 0)
+    mcbs[3].set_style_bg_color(COL_AC, 0)
+    mcbs[3].set_style_border_color(COL_HL, 0)
+    mcbs[4].set_style_bg_color(COL_AC, 0)
+    mcbs[4].set_style_border_color(COL_HL, 0)
+    mcbs[5].set_style_bg_color(COL_AC, 0)
+    mcbs[5].set_style_border_color(COL_HL, 0)
+    mcbs[6].set_style_bg_color(COL_AC, 0)
+    mcbs[6].set_style_border_color(COL_HL, 0)
+    mcbs[7].set_style_bg_color(COL_AC, 0)
+    mcbs[7].set_style_border_color(COL_HL, 0)
+    mcbs[idx].set_style_bg_color(COL_HL, 0)
+    mcbs[idx].set_style_border_color(COL_OK, 0)
+
+def action_sel_dif(idx):
+    global dif
+    dif = dstrs[idx]
+    dbs[0].set_style_bg_color(COL_AC, 0)
+    dbs[0].set_style_border_color(COL_HL, 0)
+    dbs[1].set_style_bg_color(COL_AC, 0)
+    dbs[1].set_style_border_color(COL_HL, 0)
+    dbs[2].set_style_bg_color(COL_AC, 0)
+    dbs[2].set_style_border_color(COL_HL, 0)
+    dbs[idx].set_style_bg_color(COL_HL, 0)
+    dbs[idx].set_style_border_color(COL_OK, 0)
+
+def action_show_menu():
+    mnu.clear_flag(lv.obj.FLAG.HIDDEN)
+    bring_home_top()
+    abtns[0].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[1].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[2].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[3].add_flag(lv.obj.FLAG.HIDDEN)
+    qbox.add_flag(lv.obj.FLAG.HIDDEN)
+    nbtn.add_flag(lv.obj.FLAG.HIDDEN)
+    fblbl.set_text("")
+    slbl.set_text("")
+    clbl.set_text("")
+    qlbl.set_text("")
+
+def action_show_quiz():
+    mnu.add_flag(lv.obj.FLAG.HIDDEN)
+    bring_home_top()
+    abtns[0].clear_flag(lv.obj.FLAG.HIDDEN)
+    abtns[1].clear_flag(lv.obj.FLAG.HIDDEN)
+    abtns[2].clear_flag(lv.obj.FLAG.HIDDEN)
+    abtns[3].clear_flag(lv.obj.FLAG.HIDDEN)
+    qbox.clear_flag(lv.obj.FLAG.HIDDEN)
+
+def action_show_end():
+    global end_box
+    abtns[0].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[1].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[2].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[3].add_flag(lv.obj.FLAG.HIDDEN)
+    qbox.add_flag(lv.obj.FLAG.HIDDEN)
+    nbtn.add_flag(lv.obj.FLAG.HIDDEN)
+    clbl.set_text("")
+    fblbl.set_text("")
+    n = len(q_txt)
+    pct = 0
+    if n > 0:
+        pct = (scv * 100) // n
+    eb = lv.obj(scr)
+    end_box = eb
+    eb.set_size(260, 200)
+    eb.center()
+    eb.set_style_bg_color(COL_AC, 0)
+    eb.set_style_border_color(COL_HL, 0)
+    eb.set_style_border_width(2, 0)
+    eb.set_style_radius(14, 0)
+    eb.clear_flag(lv.obj.FLAG.SCROLLABLE)
+    et = lv.label(eb)
+    et.set_text("Partie terminee !")
+    et.set_style_text_color(COL_TX, 0)
+    et.align(lv.ALIGN.TOP_MID, 0, 14)
+    es = lv.label(eb)
+    es.set_text(str(scv) + " / " + str(n) + "  (" + str(pct) + "%)")
+    es.set_style_text_color(COL_OK, 0)
+    es.align(lv.ALIGN.TOP_MID, 0, 46)
+    if pct == 100:
+        msg = "Parfait !"
+    elif pct >= 60:
+        msg = "Bien joue !"
+    elif pct >= 40:
+        msg = "Pas mal."
+    else:
+        msg = "Courage !"
+    em = lv.label(eb)
+    em.set_text(msg)
+    em.set_style_text_color(COL_DM, 0)
+    em.align(lv.ALIGN.TOP_MID, 0, 76)
+    ro = TBtn(eb, "Rejouer", 10, 150, 110, 36, "replay", 0)
+    ro.btn.set_style_bg_color(COL_OK, 0)
+    ro.btn.set_style_radius(8, 0)
+    mb = TBtn(eb, "Menu", 140, 150, 110, 36, "menu", 0)
+    mb.btn.set_style_bg_color(COL_HL, 0)
+    mb.btn.set_style_radius(8, 0)
+
+def action_show_q():
+    global aok
+    aok = False
+    if qi >= len(q_txt):
+        action_show_end()
+        return
+    qlbl.set_text(q_txt[qi])
+    clbl.set_text(q_cat[qi])
+    slbl.set_text(str(qi + 1) + "/" + str(len(q_txt)))
+    fblbl.set_text("")
+    nbtn.add_flag(lv.obj.FLAG.HIDDEN)
+    albls[0].set_text(q_a0[qi])
+    albls[1].set_text(q_a1[qi])
+    albls[2].set_text(q_a2[qi])
+    albls[3].set_text(q_a3[qi])
+    abtns[0].clear_flag(lv.obj.FLAG.HIDDEN)
+    abtns[0].set_style_bg_color(COL_AC, 0)
+    abtns[0].set_style_border_color(COL_DM, 0)
+    albls[0].set_style_text_color(COL_TX, 0)
+    abtns[1].clear_flag(lv.obj.FLAG.HIDDEN)
+    abtns[1].set_style_bg_color(COL_AC, 0)
+    abtns[1].set_style_border_color(COL_DM, 0)
+    albls[1].set_style_text_color(COL_TX, 0)
+    abtns[2].clear_flag(lv.obj.FLAG.HIDDEN)
+    abtns[2].set_style_bg_color(COL_AC, 0)
+    abtns[2].set_style_border_color(COL_DM, 0)
+    albls[2].set_style_text_color(COL_TX, 0)
+    abtns[3].clear_flag(lv.obj.FLAG.HIDDEN)
+    abtns[3].set_style_bg_color(COL_AC, 0)
+    abtns[3].set_style_border_color(COL_DM, 0)
+    albls[3].set_style_text_color(COL_TX, 0)
+
+def action_chk_ans(idx):
+    global aok, scv
+    if aok:
+        return
+    aok = True
+    ci = q_ci[qi]
+    if idx == ci:
+        scv = scv + 1
+        fblbl.set_text("Correct !")
+        fblbl.set_style_text_color(COL_OK, 0)
+    else:
+        fblbl.set_text("Faux !")
+        fblbl.set_style_text_color(COL_NO, 0)
+    if ci == 0:
+        abtns[0].set_style_bg_color(COL_OK, 0)
+        albls[0].set_style_text_color(COL_TX, 0)
+    elif idx == 0:
+        abtns[0].set_style_bg_color(COL_NO, 0)
+        albls[0].set_style_text_color(COL_TX, 0)
+    if ci == 1:
+        abtns[1].set_style_bg_color(COL_OK, 0)
+        albls[1].set_style_text_color(COL_TX, 0)
+    elif idx == 1:
+        abtns[1].set_style_bg_color(COL_NO, 0)
+        albls[1].set_style_text_color(COL_TX, 0)
+    if ci == 2:
+        abtns[2].set_style_bg_color(COL_OK, 0)
+        albls[2].set_style_text_color(COL_TX, 0)
+    elif idx == 2:
+        abtns[2].set_style_bg_color(COL_NO, 0)
+        albls[2].set_style_text_color(COL_TX, 0)
+    if ci == 3:
+        abtns[3].set_style_bg_color(COL_OK, 0)
+        albls[3].set_style_text_color(COL_TX, 0)
+    elif idx == 3:
+        abtns[3].set_style_bg_color(COL_NO, 0)
+        albls[3].set_style_text_color(COL_TX, 0)
+    nbtn.clear_flag(lv.obj.FLAG.HIDDEN)
+
+def action_next():
+    global qi
+    qi = qi + 1
+    action_show_q()
+
+def action_replay_after_end():
+    global end_box
+    if end_box != 0:
+        end_box = 0
+    action_start_game()
+
+def action_menu_after_end():
+    global end_box
+    if end_box != 0:
+        end_box = 0
+    action_show_menu()
+
+def action_do_fetch(t):
+    global q_txt, q_a0, q_a1, q_a2, q_a3, q_ci, q_cat
+    global qi, scv, ft, _rs
+    t._del()
+    ft = 0
+    url = "http://opentdb.com/api.php?amount=" + str(QMAX)
+    url = url + "&category=" + str(cid)
+    url = url + "&difficulty=" + dif
+    url = url + "&type=multiple"
+    q_txt = []
+    q_a0 = []
+    q_a1 = []
+    q_a2 = []
+    q_a3 = []
+    q_ci = []
+    q_cat = []
+    raw = lv.http_get(url)
+    if not raw:
+        qlbl.set_text("Erreur reseau.")
+        slbl.set_text("")
+        return
+    if len(raw) < 10:
+        qlbl.set_text("Reponse vide.")
+        slbl.set_text("")
+        return
+    data = jparse(raw)
+    if not data:
+        qlbl.set_text("Erreur parsing.")
+        slbl.set_text("")
+        return
+    rc = data["response_code"]
+    if rc != 0:
+        qlbl.set_text("Erreur API.")
+        slbl.set_text("")
+        return
+    results = data["results"]
+    if not results:
+        qlbl.set_text("Aucune question.")
+        slbl.set_text("")
+        return
+    ri = 0
+    while ri < len(results):
+        r = results[ri]
+        ri = ri + 1
+        if r:
+            q = dhtml(r["question"])
+            cor = dhtml(r["correct_answer"])
+            inc = r["incorrect_answers"]
+            if q and cor and len(inc) >= 3:
+                ch = [cor, dhtml(inc[0]), dhtml(inc[1]), dhtml(inc[2])]
+                _rs = _rs + len(q)
+                shuf(ch)
+                ci = 0
+                if ch[1] == cor:
+                    ci = 1
+                elif ch[2] == cor:
+                    ci = 2
+                elif ch[3] == cor:
+                    ci = 3
+                cat = dhtml(r["category"])
+                q_txt.append(q)
+                q_a0.append(ch[0])
+                q_a1.append(ch[1])
+                q_a2.append(ch[2])
+                q_a3.append(ch[3])
+                q_ci.append(ci)
+                q_cat.append(cat)
+    if len(q_txt) == 0:
+        qlbl.set_text("Erreur traitement.")
+        slbl.set_text("")
+        return
+    qi = 0
+    scv = 0
+    action_show_q()
+
+def action_start_game():
+    global ft
+    action_show_quiz()
+    qlbl.set_text("Chargement...")
+    slbl.set_text("")
+    clbl.set_text("")
+    fblbl.set_text("")
+    nbtn.add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[0].set_style_bg_color(COL_AC, 0)
+    abtns[0].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[1].set_style_bg_color(COL_AC, 0)
+    abtns[1].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[2].set_style_bg_color(COL_AC, 0)
+    abtns[2].add_flag(lv.obj.FLAG.HIDDEN)
+    abtns[3].set_style_bg_color(COL_AC, 0)
+    abtns[3].add_flag(lv.obj.FLAG.HIDDEN)
+    if ft != 0:
+        ft._del()
+        ft = 0
+    ft = lv.timer_create_basic()
+    ft.set_period(100)
+    ft.set_cb(action_do_fetch)
+
 class TBtn:
     def __init__(self, parent, text, x, y, w, h, kind, idx):
         b = lv.btn(parent)
@@ -231,23 +524,23 @@ class TBtn:
         if self.kind == "home":
             oh(0)
         elif self.kind == "cat":
-            sel_cat(self.idx)
+            action_sel_cat(self.idx)
         elif self.kind == "dif":
-            sel_dif(self.idx)
+            action_sel_dif(self.idx)
         elif self.kind == "ans":
-            chk_ans(self.idx)
+            action_chk_ans(self.idx)
         elif self.kind == "next":
-            on_nxt(0)
+            action_next()
         elif self.kind == "play":
-            start_game()
+            action_start_game()
         elif self.kind == "replay":
             if end_box != 0:
                 end_box._del()
-            replay_after_end()
+            action_replay_after_end()
         elif self.kind == "menu":
             if end_box != 0:
                 end_box._del()
-            menu_after_end()
+            action_menu_after_end()
 
 def bring_home_top():
     hb.move_foreground()
