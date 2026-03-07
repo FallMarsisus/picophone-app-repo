29e9b653ea07b9b55ca23825594080265bf0d126
Marsisus <simonlncln@gmail.com>
2026-03-08 00:27:48 +0100

    vraiment proutesque

--- apps/trivia/main.py
+++ apps/trivia/main.py
@@ -27,15 +27,12 @@ ft = 0
 cid = 9
 dif = "easy"
 _rs = 12345
-tap_refs = []
 ebox = 0
 
 cids = [9, 17, 21, 23, 11, 12, 15, 18]
 cnms = ["Culture G.", "Sciences", "Sports", "Histoire", "Cinema", "Musique", "Jeux Video", "Informatique"]
 dstrs = ["easy", "medium", "hard"]
 dnms = ["Facile", "Moyen", "Difficile"]
-
-# ---- HTML decode ----
 
 def dhtml(s):
     o = ""
@@ -75,8 +72,6 @@ def dhtml(s):
             o = o + s[i]
             i = i + 1
     return o
-
-# ---- JSON parser ----
 
 _js = ""
 _jp = 0
@@ -132,6 +127,8 @@ def _jval():
     c = _js[_jp]
     if c == '"':
         return _jstr()
+    if c == '{':
+        return _jobj()
     if c == '[':
         return _jarr()
     if c == 't':
@@ -143,8 +140,6 @@ def _jval():
     if c == 'n':
         _jp = _jp + 4
         return ""
-    if c == '{':
-        return _jobj()
     return _jnum()
 
 def _jobj():
@@ -195,8 +190,6 @@ def jparse(s):
     _jp = 0
     return _jval()
 
-# ---- PRNG ----
-
 def _rnd():
     global _rs
     _rs = (_rs * 1103515245 + 12345) % 2147483648
@@ -212,7 +205,23 @@ def shuf(arr):
         arr[j] = tmp
         i = i - 1
 
-# ---- Home timer ----
+# ---- Screen ----
+
+scr = lv.scr_act()
+scr.clear_flag(lv.obj.FLAG.SCROLLABLE)
+scr.set_style_bg_color(COL_BG, 0)
+
+# ---- Home button (same pattern as Wordle) ----
+
+hb = lv.btn(scr)
+hb.set_size(60, 28)
+hb.align(lv.ALIGN.TOP_LEFT, 4, 4)
+hb.set_style_bg_color(COL_AC, 0)
+hb.set_style_radius(6, 0)
+hl = lv.label(hb)
+hl.set_text("< Home")
+hl.set_style_text_color(COL_TX, 0)
+hl.center()
 
 def dq(t):
     global qt
@@ -226,7 +235,173 @@ def oh(evt):
     qt.set_period(60)
     qt.set_cb(dq)
 
-# ---- Game logic ----
+hb.add_event_cb(oh, lv.EVENT.CLICKED, 0)
+
+# ---- Title ----
+
+tl = lv.label(scr)
+tl.set_text("Trivia Quiz")
+tl.set_style_text_color(COL_TX, 0)
+tl.align(lv.ALIGN.TOP_MID, 0, 6)
+
+# ---- Score label ----
+
+slbl = lv.label(scr)
+slbl.set_text("")
+slbl.set_style_text_color(COL_DM, 0)
+slbl.align(lv.ALIGN.TOP_RIGHT, -8, 8)
+
+# ---- Category label ----
+
+clbl = lv.label(scr)
+clbl.set_text("")
+clbl.set_style_text_color(COL_DM, 0)
+clbl.align(lv.ALIGN.TOP_MID, 0, 28)
+
+# ---- Question box ----
+
+qbox = lv.obj(scr)
+qbox.set_size(300, 100)
+qbox.align(lv.ALIGN.TOP_MID, 0, 46)
+qbox.set_style_bg_color(COL_AC, 0)
+qbox.set_style_border_width(0, 0)
+qbox.set_style_radius(10, 0)
+qbox.clear_flag(lv.obj.FLAG.SCROLLABLE)
+
+qlbl = lv.label(qbox)
+qlbl.set_text("")
+qlbl.set_style_text_color(COL_TX, 0)
+qlbl.align(lv.ALIGN.TOP_LEFT, 10, 8)
+
+# ---- Answer buttons (plain lv.btn like dummy app) ----
+
+abtns = []
+albls = []
+ai = 0
+while ai < 4:
+    by = 154 + ai * 56
+    ab = lv.btn(scr)
+    ab.set_size(296, 48)
+    ab.align(lv.ALIGN.TOP_LEFT, 12, by)
+    ab.set_style_bg_color(COL_AC, 0)
+    ab.set_style_radius(10, 0)
+    ab.set_style_border_color(COL_DM, 0)
+    ab.set_style_border_width(1, 0)
+    alb = lv.label(ab)
+    alb.set_text("")
+    alb.set_style_text_color(COL_TX, 0)
+    alb.center()
+    abtns.append(ab)
+    albls.append(alb)
+    ai = ai + 1
+
+# ---- Feedback ----
+
+fblbl = lv.label(scr)
+fblbl.set_text("")
+fblbl.set_style_text_color(COL_TX, 0)
+fblbl.align(lv.ALIGN.TOP_MID, 0, 382)
+
+# ---- Next button ----
+
+nbtn = lv.btn(scr)
+nbtn.set_size(140, 40)
+nbtn.align(lv.ALIGN.TOP_LEFT, 90, 406)
+nbtn.set_style_bg_color(COL_HL, 0)
+nbtn.set_style_radius(10, 0)
+nbtn.add_flag(lv.obj.FLAG.HIDDEN)
+nlbl = lv.label(nbtn)
+nlbl.set_text("Suivant >")
+nlbl.set_style_text_color(COL_TX, 0)
+nlbl.center()
+
+# ---- Menu ----
+
+mnu = lv.obj(scr)
+mnu.set_size(SCR_W, SCR_H)
+mnu.align(lv.ALIGN.TOP_LEFT, 0, 0)
+mnu.set_style_bg_color(COL_BG, 0)
+mnu.set_style_border_width(0, 0)
+mnu.clear_flag(lv.obj.FLAG.SCROLLABLE)
+hb.move_foreground()
+
+mtl = lv.label(mnu)
+mtl.set_text("Trivia Quiz")
+mtl.set_style_text_color(COL_TX, 0)
+mtl.align(lv.ALIGN.TOP_MID, 0, 40)
+
+msl = lv.label(mnu)
+msl.set_text("Choisis une categorie :")
+msl.set_style_text_color(COL_DM, 0)
+msl.align(lv.ALIGN.TOP_MID, 0, 70)
+
+# ---- Category buttons ----
+
+mcbs = []
+ci = 0
+while ci < 8:
+    col = ci % 2
+    row = ci // 2
+    cx = 42 + col * 148
+    cy = 100 + row * 44
+    txt = cnms[ci]
+    cb = lv.btn(mnu)
+    cb.set_size(130, 36)
+    cb.align(lv.ALIGN.TOP_LEFT, cx, cy)
+    cb.set_style_bg_color(COL_AC, 0)
+    cb.set_style_radius(8, 0)
+    cb.set_style_border_color(COL_HL, 0)
+    cb.set_style_border_width(1, 0)
+    clb = lv.label(cb)
+    clb.set_text(txt)
+    clb.set_style_text_color(COL_TX, 0)
+    clb.center()
+    mcbs.append(cb)
+    ci = ci + 1
+
+# ---- Difficulty ----
+
+ddl = lv.label(mnu)
+ddl.set_text("Difficulte :")
+ddl.set_style_text_color(COL_DM, 0)
+ddl.align(lv.ALIGN.TOP_MID, 0, 282)
+
+dbs = []
+di = 0
+while di < 3:
+    dx = 18 + di * 98
+    txt = dnms[di]
+    dbt = lv.btn(mnu)
+    dbt.set_size(90, 34)
+    dbt.align(lv.ALIGN.TOP_LEFT, dx, 306)
+    dbt.set_style_radius(8, 0)
+    dbt.set_style_border_width(1, 0)
+    if di == 0:
+        dbt.set_style_bg_color(COL_HL, 0)
+        dbt.set_style_border_color(COL_OK, 0)
+    else:
+        dbt.set_style_bg_color(COL_AC, 0)
+        dbt.set_style_border_color(COL_HL, 0)
+    dlb = lv.label(dbt)
+    dlb.set_text(txt)
+    dlb.set_style_text_color(COL_TX, 0)
+    dlb.center()
+    dbs.append(dbt)
+    di = di + 1
+
+# ---- Play button ----
+
+pbtn = lv.btn(mnu)
+pbtn.set_size(200, 46)
+pbtn.align(lv.ALIGN.TOP_LEFT, 60, 366)
+pbtn.set_style_bg_color(COL_OK, 0)
+pbtn.set_style_radius(10, 0)
+plbl = lv.label(pbtn)
+plbl.set_text("Jouer !")
+plbl.set_style_text_color(COL_TX, 0)
+plbl.center()
+
+# ==== GAME FUNCTIONS (all UI vars exist above) ====
 
 def sel_cat(idx):
     global cid
@@ -273,6 +448,71 @@ def sv():
         i = i + 1
     qbox.clear_flag(lv.obj.FLAG.HIDDEN)
 
+def ca(idx):
+    global aok, scv
+    if aok:
+        return
+    aok = True
+    ci = q_ci[qi]
+    if idx == ci:
+        scv = scv + 1
+        fblbl.set_text("Correct !")
+        fblbl.set_style_text_color(COL_OK, 0)
+    else:
+        fblbl.set_text("Faux !")
+        fblbl.set_style_text_color(COL_NO, 0)
+    j = 0
+    while j < 4:
+        if ci == j:
+            abtns[j].set_style_bg_color(COL_OK, 0)
+            albls[j].set_style_text_color(COL_TX, 0)
+        elif idx == j:
+            abtns[j].set_style_bg_color(COL_NO, 0)
+            albls[j].set_style_text_color(COL_TX, 0)
+        j = j + 1
+    nbtn.clear_flag(lv.obj.FLAG.HIDDEN)
+
+def shq():
+    global aok
+    aok = False
+    qc = len(q_txt)
+    if qi >= qc:
+        se()
+        return
+    qtxt = q_txt[qi]
+    qlbl.set_text(qtxt)
+    ctxt = q_cat[qi]
+    clbl.set_text(ctxt)
+    qn = qi + 1
+    s1 = str(qn)
+    s2 = str(qc)
+    stxt = s1 + "/" + s2
+    slbl.set_text(stxt)
+    fblbl.set_text("")
+    nbtn.add_flag(lv.obj.FLAG.HIDDEN)
+    albls[0].set_text(q_a0[qi])
+    albls[1].set_text(q_a1[qi])
+    albls[2].set_text(q_a2[qi])
+    albls[3].set_text(q_a3[qi])
+    i = 0
+    while i < 4:
+        abtns[i].clear_flag(lv.obj.FLAG.HIDDEN)
+        abtns[i].set_style_bg_color(COL_AC, 0)
+        abtns[i].set_style_border_color(COL_DM, 0)
+        albls[i].set_style_text_color(COL_TX, 0)
+        i = i + 1
+
+def nxt():
+    global qi
+    qi = qi + 1
+    shq()
+
+def on_rep(evt):
+    sg()
+
+def on_mnu_end(evt):
+    sm()
+
 def se():
     global ebox
     i = 0
@@ -301,7 +541,11 @@ def se():
     et.set_style_text_color(COL_TX, 0)
     et.align(lv.ALIGN.TOP_MID, 0, 14)
     es = lv.label(eb)
-    es.set_text(str(scv) + " / " + str(n) + "  (" + str(pct) + "%)")
+    s1 = str(scv)
+    s2 = str(n)
+    s3 = str(pct)
+    stxt = s1 + " / " + s2 + "  (" + s3 + "%)"
+    es.set_text(stxt)
     es.set_style_text_color(COL_OK, 0)
     es.align(lv.ALIGN.TOP_MID, 0, 46)
     if pct == 100:
@@ -325,90 +569,29 @@ def se():
     rl.set_text("Rejouer")
     rl.set_style_text_color(COL_TX, 0)
     rl.center()
-    def on_rep(evt):
-        global ebox
-        if ebox != 0:
-            ebox._del()
-            ebox = 0
-        sg()
     rb.add_event_cb(on_rep, lv.EVENT.CLICKED, 0)
-    mb = lv.btn(eb)
-    mb.set_size(110, 36)
-    mb.align(lv.ALIGN.TOP_LEFT, 140, 150)
-    mb.set_style_bg_color(COL_HL, 0)
-    mb.set_style_radius(8, 0)
-    ml = lv.label(mb)
-    ml.set_text("Menu")
-    ml.set_style_text_color(COL_TX, 0)
-    ml.center()
-    def on_mnu(evt):
-        global ebox
-        if ebox != 0:
-            ebox._del()
-            ebox = 0
-        sm()
-    mb.add_event_cb(on_mnu, lv.EVENT.CLICKED, 0)
-
-def shq():
-    global aok
-    aok = False
-    if qi >= len(q_txt):
-        se()
-        return
-    qlbl.set_text(q_txt[qi])
-    clbl.set_text(q_cat[qi])
-    slbl.set_text(str(qi + 1) + "/" + str(len(q_txt)))
-    fblbl.set_text("")
-    nbtn.add_flag(lv.obj.FLAG.HIDDEN)
-    albls[0].set_text(q_a0[qi])
-    albls[1].set_text(q_a1[qi])
-    albls[2].set_text(q_a2[qi])
-    albls[3].set_text(q_a3[qi])
-    i = 0
-    while i < 4:
-        abtns[i].clear_flag(lv.obj.FLAG.HIDDEN)
-        abtns[i].set_style_bg_color(COL_AC, 0)
-        abtns[i].set_style_border_color(COL_DM, 0)
-        albls[i].set_style_text_color(COL_TX, 0)
-        i = i + 1
-
-def ca(idx):
-    global aok, scv
-    if aok:
-        return
-    aok = True
-    ci = q_ci[qi]
-    if idx == ci:
-        scv = scv + 1
-        fblbl.set_text("Correct !")
-        fblbl.set_style_text_color(COL_OK, 0)
-    else:
-        fblbl.set_text("Faux !")
-        fblbl.set_style_text_color(COL_NO, 0)
-    j = 0
-    while j < 4:
-        if ci == j:
-            abtns[j].set_style_bg_color(COL_OK, 0)
-            albls[j].set_style_text_color(COL_TX, 0)
-        elif idx == j:
-            abtns[j].set_style_bg_color(COL_NO, 0)
-            albls[j].set_style_text_color(COL_TX, 0)
-        j = j + 1
-    nbtn.clear_flag(lv.obj.FLAG.HIDDEN)
-
-def nxt():
-    global qi
-    qi = qi + 1
-    shq()
+    mbx = lv.btn(eb)
+    mbx.set_size(110, 36)
+    mbx.align(lv.ALIGN.TOP_LEFT, 140, 150)
+    mbx.set_style_bg_color(COL_HL, 0)
+    mbx.set_style_radius(8, 0)
+    mbl = lv.label(mbx)
+    mbl.set_text("Menu")
+    mbl.set_style_text_color(COL_TX, 0)
+    mbl.center()
+    mbx.add_event_cb(on_mnu_end, lv.EVENT.CLICKED, 0)
 
 def df(t):
     global q_txt, q_a0, q_a1, q_a2, q_a3, q_ci, q_cat
     global qi, scv, ft, _rs
     t._del()
     ft = 0
-    url = "http://opentdb.com/api.php?amount=" + str(QMAX)
-    url = url + "&category=" + str(cid)
-    url = url + "&difficulty=" + dif
+    url = "http://opentdb.com/api.php?amount="
+    url = url + str(QMAX)
+    url = url + "&category="
+    url = url + str(cid)
+    url = url + "&difficulty="
+    url = url + dif
     url = url + "&type=multiple"
     q_txt = []
     q_a0 = []
@@ -422,7 +605,8 @@ def df(t):
         qlbl.set_text("Erreur reseau.")
         slbl.set_text("")
         return
-    if len(raw) < 10:
+    rlen = len(raw)
+    if rlen < 10:
         qlbl.set_text("Reponse vide.")
         slbl.set_text("")
         return
@@ -442,7 +626,8 @@ def df(t):
         slbl.set_text("")
         return
     ri = 0
-    while ri < len(results):
+    rlen = len(results)
+    while ri < rlen:
         r = results[ri]
         ri = ri + 1
         if r:
@@ -451,7 +636,8 @@ def df(t):
             inc = r["incorrect_answers"]
             q = dhtml(rq)
             cor = dhtml(ra)
-            if q and cor and len(inc) >= 3:
+            inclen = len(inc)
+            if q and cor and inclen >= 3:
                 t0 = inc[0]
                 t1 = inc[1]
                 t2 = inc[2]
@@ -459,7 +645,8 @@ def df(t):
                 i1 = dhtml(t1)
                 i2 = dhtml(t2)
                 ch = [cor, i0, i1, i2]
-                _rs = _rs + len(q)
+                qlen = len(q)
+                _rs = _rs + qlen
                 shuf(ch)
                 ci = 0
                 if ch[1] == cor:
@@ -477,7 +664,8 @@ def df(t):
                 q_a3.append(ch[3])
                 q_ci.append(ci)
                 q_cat.append(cat)
-    if len(q_txt) == 0:
+    qcount = len(q_txt)
+    if qcount == 0:
         qlbl.set_text("Erreur traitement.")
         slbl.set_text("")
         return
@@ -486,7 +674,10 @@ def df(t):
     shq()
 
 def sg():
-    global ft
+    global ft, ebox
+    if ebox != 0:
+        ebox._del()
+        ebox = 0
     sv()
     qlbl.set_text("Chargement...")
     slbl.set_text("")
@@ -505,166 +696,81 @@ def sg():
     ft.set_period(100)
     ft.set_cb(df)
 
-# ---- Button class (same as Wordle K) ----
+# ==== CALLBACKS (all game functions exist above) ====
 
-class TBtn:
-    def __init__(self, p, t, x, y, w, h, kind, idx):
-        b = lv.btn(p)
-        b.set_size(w, h)
-        b.align(lv.ALIGN.TOP_LEFT, x, y)
-        l = lv.label(b)
-        l.set_text(t)
-        l.set_style_text_color(COL_TX, 0)
-        l.center()
-        self.btn = b
-        self.lbl = l
-        self.kind = kind
-        self.idx = idx
-        b.add_event_cb(self.oc, lv.EVENT.CLICKED, None)
-        tap_refs.append(self)
+def oa0(evt):
+    ca(0)
 
-    def oc(self, e):
-        if self.kind == "home":
-            oh(0)
-        elif self.kind == "cat":
-            sel_cat(self.idx)
-        elif self.kind == "dif":
-            sel_dif(self.idx)
-        elif self.kind == "ans":
-            ca(self.idx)
-        elif self.kind == "next":
-            nxt()
-        elif self.kind == "play":
-            sg()
+def oa1(evt):
+    ca(1)
 
-# ---- UI (like Wordle) ----
+def oa2(evt):
+    ca(2)
 
-scr = lv.scr_act()
-scr.clear_flag(lv.obj.FLAG.SCROLLABLE)
-scr.set_style_bg_color(COL_BG, 0)
+def oa3(evt):
+    ca(3)
 
-# Home button (like Wordle: plain lv.btn, not class)
-hb = lv.btn(scr)
-hb.set_size(60, 28)
-hb.align(lv.ALIGN.TOP_LEFT, 4, 4)
-hb.set_style_bg_color(COL_AC, 0)
-hb.set_style_radius(6, 0)
-hl = lv.label(hb)
-hl.set_text("< Home")
-hl.set_style_text_color(COL_TX, 0)
-hl.center()
-hb.add_event_cb(oh, lv.EVENT.CLICKED, 0)
+abtns[0].add_event_cb(oa0, lv.EVENT.CLICKED, 0)
+abtns[1].add_event_cb(oa1, lv.EVENT.CLICKED, 0)
+abtns[2].add_event_cb(oa2, lv.EVENT.CLICKED, 0)
+abtns[3].add_event_cb(oa3, lv.EVENT.CLICKED, 0)
 
-tl = lv.label(scr)
-tl.set_text("Trivia Quiz")
-tl.set_style_text_color(COL_TX, 0)
-tl.align(lv.ALIGN.TOP_MID, 0, 6)
+def onx(evt):
+    nxt()
 
-slbl = lv.label(scr)
-slbl.set_text("")
-slbl.set_style_text_color(COL_DM, 0)
-slbl.align(lv.ALIGN.TOP_RIGHT, -8, 8)
+nbtn.add_event_cb(onx, lv.EVENT.CLICKED, 0)
 
-clbl = lv.label(scr)
-clbl.set_text("")
-clbl.set_style_text_color(COL_DM, 0)
-clbl.align(lv.ALIGN.TOP_MID, 0, 28)
+def oc0(evt):
+    sel_cat(0)
 
-qbox = lv.obj(scr)
-qbox.set_size(300, 100)
-qbox.align(lv.ALIGN.TOP_MID, 0, 46)
-qbox.set_style_bg_color(COL_AC, 0)
-qbox.set_style_border_width(0, 0)
-qbox.set_style_radius(10, 0)
-qbox.clear_flag(lv.obj.FLAG.SCROLLABLE)
+def oc1(evt):
+    sel_cat(1)
 
-qlbl = lv.label(qbox)
-qlbl.set_text("")
-qlbl.set_style_text_color(COL_TX, 0)
-qlbl.align(lv.ALIGN.TOP_LEFT, 10, 8)
+def oc2(evt):
+    sel_cat(2)
 
-abtns = []
-albls = []
-ai = 0
-while ai < 4:
-    by = 154 + ai * 56
-    ao = TBtn(scr, "", 12, by, 296, 48, "ans", ai)
-    ao.btn.set_style_bg_color(COL_AC, 0)
-    ao.btn.set_style_radius(10, 0)
-    ao.btn.set_style_border_color(COL_DM, 0)
-    ao.btn.set_style_border_width(1, 0)
-    abtns.append(ao.btn)
-    albls.append(ao.lbl)
-    ai = ai + 1
+def oc3(evt):
+    sel_cat(3)
 
-fblbl = lv.label(scr)
-fblbl.set_text("")
-fblbl.set_style_text_color(COL_TX, 0)
-fblbl.align(lv.ALIGN.TOP_MID, 0, 382)
+def oc4(evt):
+    sel_cat(4)
 
-_n = TBtn(scr, "Suivant >", 90, 406, 140, 40, "next", 0)
-nbtn = _n.btn
-nbtn.set_style_bg_color(COL_HL, 0)
-nbtn.set_style_radius(10, 0)
-nbtn.add_flag(lv.obj.FLAG.HIDDEN)
+def oc5(evt):
+    sel_cat(5)
 
-mnu = lv.obj(scr)
-mnu.set_size(SCR_W, SCR_H)
-mnu.align(lv.ALIGN.TOP_LEFT, 0, 0)
-mnu.set_style_bg_color(COL_BG, 0)
-mnu.set_style_border_width(0, 0)
-mnu.clear_flag(lv.obj.FLAG.SCROLLABLE)
-hb.move_foreground()
+def oc6(evt):
+    sel_cat(6)
 
-mtl = lv.label(mnu)
-mtl.set_text("Trivia Quiz")
-mtl.set_style_text_color(COL_TX, 0)
-mtl.align(lv.ALIGN.TOP_MID, 0, 40)
+def oc7(evt):
+    sel_cat(7)
 
-msl = lv.label(mnu)
-msl.set_text("Choisis une categorie :")
-msl.set_style_text_color(COL_DM, 0)
-msl.align(lv.ALIGN.TOP_MID, 0, 70)
+mcbs[0].add_event_cb(oc0, lv.EVENT.CLICKED, 0)
+mcbs[1].add_event_cb(oc1, lv.EVENT.CLICKED, 0)
+mcbs[2].add_event_cb(oc2, lv.EVENT.CLICKED, 0)
+mcbs[3].add_event_cb(oc3, lv.EVENT.CLICKED, 0)
+mcbs[4].add_event_cb(oc4, lv.EVENT.CLICKED, 0)
+mcbs[5].add_event_cb(oc5, lv.EVENT.CLICKED, 0)
+mcbs[6].add_event_cb(oc6, lv.EVENT.CLICKED, 0)
+mcbs[7].add_event_cb(oc7, lv.EVENT.CLICKED, 0)
 
-mcbs = []
-ci = 0
-while ci < 8:
-    col = ci % 2
-    row = ci // 2
-    cx = 42 + col * 148
-    cy = 100 + row * 44
-    co = TBtn(mnu, cnms[ci], cx, cy, 130, 36, "cat", ci)
-    co.btn.set_style_bg_color(COL_AC, 0)
-    co.btn.set_style_radius(8, 0)
-    co.btn.set_style_border_color(COL_HL, 0)
-    co.btn.set_style_border_width(1, 0)
-    mcbs.append(co.btn)
-    ci = ci + 1
+def od0(evt):
+    sel_dif(0)
 
-dll = lv.label(mnu)
-dll.set_text("Difficulte :")
-dll.set_style_text_color(COL_DM, 0)
-dll.align(lv.ALIGN.TOP_MID, 0, 282)
+def od1(evt):
+    sel_dif(1)
 
-dbs = []
-di = 0
-while di < 3:
-    dx = 18 + di * 98
-    db = TBtn(mnu, dnms[di], dx, 306, 90, 34, "dif", di)
-    db.btn.set_style_radius(8, 0)
-    db.btn.set_style_border_width(1, 0)
-    if di == 0:
-        db.btn.set_style_bg_color(COL_HL, 0)
-        db.btn.set_style_border_color(COL_OK, 0)
-    else:
-        db.btn.set_style_bg_color(COL_AC, 0)
-        db.btn.set_style_border_color(COL_HL, 0)
-    dbs.append(db.btn)
-    di = di + 1
+def od2(evt):
+    sel_dif(2)
 
-_p = TBtn(mnu, "Jouer !", 60, 366, 200, 46, "play", 0)
-pbtn = _p.btn
-pbtn.set_style_bg_color(COL_OK, 0)
-pbtn.set_style_radius(10, 0)
+dbs[0].add_event_cb(od0, lv.EVENT.CLICKED, 0)
+dbs[1].add_event_cb(od1, lv.EVENT.CLICKED, 0)
+dbs[2].add_event_cb(od2, lv.EVENT.CLICKED, 0)
+
+def op(evt):
+    sg()
+
+pbtn.add_event_cb(op, lv.EVENT.CLICKED, 0)
+
+# ==== START ====
 
 sm()
