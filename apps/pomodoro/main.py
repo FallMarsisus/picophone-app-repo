d3e05b86b38b6edf32a7769e3c7afef7fabcc51e
Marsisus <simonlncln@gmail.com>
2026-03-08 10:22:22 +0100

    debug pomo

--- apps/pomodoro/main.py
+++ apps/pomodoro/main.py
@@ -12,41 +12,47 @@ main_timer = 0
 
 # --- INTERFACE GRAPHIQUE ---
 scr = lv.scr_act()
-# Fond d'écran sombre élégant
-scr.set_style_bg_color(lv.color_hex(0x1A1A3E), 0)
+scr.clear_flag(lv.obj.FLAG.SCROLLABLE)
+scr.set_style_bg_color(lv.color_black(), 0)
 
 # Titre
 title = lv.label(scr)
 title.set_text("POMODORO")
-title.set_style_text_color(lv.color_hex(0x00D9FF), 0)
+title.set_style_text_color(lv.palette_main(lv.PALETTE.CYAN), 0)
 title.align(lv.ALIGN.TOP_MID, 0, 20)
 
 # Statut (Travail ou Pause)
 status_lbl = lv.label(scr)
 status_lbl.set_text("TRAVAIL")
-status_lbl.set_style_text_color(lv.color_hex(0xFF5555), 0) # Rouge
+status_lbl.set_style_text_color(lv.palette_main(lv.PALETTE.RED), 0)
 status_lbl.align(lv.ALIGN.TOP_MID, 0, 50)
 
 # Affichage du temps restant
 time_lbl = lv.label(scr)
 time_lbl.set_text("25:00")
-time_lbl.set_style_text_color(lv.color_hex(0xFFFFFF), 0)
+time_lbl.set_style_text_color(lv.color_white(), 0)
 time_lbl.align(lv.ALIGN.CENTER, 0, -50)
 
 # Fonction pour formater les secondes en MM:SS
 def format_time(seconds):
     m = seconds // 60
     s = seconds % 60
-    return "%02d:%02d" % (m, s)
+    ms = str(m)
+    ss = str(s)
+    if m < 10:
+        ms = "0" + ms
+    if s < 10:
+        ss = "0" + ss
+    return ms + ":" + ss
 
 def update_ui():
     time_lbl.set_text(format_time(time_left))
     if is_work:
         status_lbl.set_text("TRAVAIL")
-        status_lbl.set_style_text_color(lv.color_hex(0xFF5555), 0)
+        status_lbl.set_style_text_color(lv.palette_main(lv.PALETTE.RED), 0)
     else:
         status_lbl.set_text("PAUSE")
-        status_lbl.set_style_text_color(lv.color_hex(0x55FF55), 0)
+        status_lbl.set_style_text_color(lv.palette_main(lv.PALETTE.GREEN), 0)
 
 # --- LE CHRONOMÈTRE (Tick chaque seconde) ---
 def on_tick(t):
@@ -89,7 +95,7 @@ def on_start(evt):
     is_running = not is_running
     if is_running:
         start_lbl.set_text("Pause")
-        start_btn.set_style_bg_color(lv.color_hex(0xCCAA00), 0) # Devient jaune en pause
+        start_btn.set_style_bg_color(lv.palette_main(lv.PALETTE.YELLOW), 0)
     else:
         start_lbl.set_text("Start")
         start_btn.set_style_bg_color(lv.palette_main(lv.PALETTE.BLUE), 0)
@@ -100,7 +106,7 @@ start_btn.add_event_cb(on_start, lv.EVENT.CLICKED, 0)
 reset_btn = lv.btn(scr)
 reset_btn.set_size(120, 50)
 reset_btn.align(lv.ALIGN.CENTER, 70, 30)
-reset_btn.set_style_bg_color(lv.color_hex(0x555555), 0)
+reset_btn.set_style_bg_color(lv.palette_main(lv.PALETTE.GREY), 0)
 reset_lbl = lv.label(reset_btn)
 reset_lbl.set_text("Reset")
 reset_lbl.center()
@@ -123,7 +129,7 @@ reset_btn.add_event_cb(on_reset, lv.EVENT.CLICKED, 0)
 skip_btn = lv.btn(scr)
 skip_btn.set_size(260, 40)
 skip_btn.align(lv.ALIGN.BOTTOM_MID, 0, -80)
-skip_btn.set_style_bg_color(lv.color_hex(0x444466), 0)
+skip_btn.set_style_bg_color(lv.palette_main(lv.PALETTE.DEEP_PURPLE), 0)
 skip_lbl = lv.label(skip_btn)
 skip_lbl.set_text("Forcer l'autre phase >>")
 skip_lbl.center()
@@ -147,7 +153,7 @@ skip_btn.add_event_cb(on_skip, lv.EVENT.CLICKED, 0)
 quit_btn = lv.btn(scr)
 quit_btn.set_size(260, 45)
 quit_btn.align(lv.ALIGN.BOTTOM_MID, 0, -15)
-quit_btn.set_style_bg_color(lv.color_hex(0xAA3333), 0)
+quit_btn.set_style_bg_color(lv.palette_main(lv.PALETTE.RED), 0)
 quit_lbl = lv.label(quit_btn)
 quit_lbl.set_text("Quitter vers Home")
 quit_lbl.center()
